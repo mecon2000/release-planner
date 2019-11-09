c337e1362173b71e7506049b2ecb008875c429c6
Ron Ganot <rganot@aligntech.com>
2019-11-10 01:34:49 +0200

    added tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
 import './App.css';
@@ -7,60 +14,112 @@ import { db } from './release_planner_db.js';
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 
 
-function App() {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div className="App">
-      <h1>Groups:</h1>
-      <div className="responsiveTable"></div>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {db.groups.data.map((groupName, index) => {
-            return (
-              <Tr>
-                <Td key={index}>{groupName}</Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-
-      <hr />
-
-      <h1>Teams:</h1>
-      <div className="responsiveTable"></div>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Belongs to</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {db.teams.data.map((team, index) => {
-            return (
-              <Tr>
-                <Td key={index}>{team.name}</Td>
-                <Td key={index + db.teams.data.length}>{team.group}</Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-
-      {db.teams.enableEditing ? (
-        <Button variant="contained" color="primary">
-          Add
-      </Button>) : ""}
-
-    </div >
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
   );
 }
 
-export default App;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: 'black',
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Groups" {...a11yProps(0)} />
+          <Tab label="Teams" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <div className="responsiveTable"></div>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {db.groups.data.map((groupName, index) => {
+              return (
+                <Tr>
+                  <Td key={index}>{groupName}</Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TabPanel>
 
 
+      <TabPanel value={value} index={1}>
+        <div className="responsiveTable"></div>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Belongs to</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {db.teams.data.map((team, index) => {
+              return (
+                <Tr>
+                  <Td key={index}>{team.name}</Td>
+                  <Td key={index + db.teams.data.length}>{team.group}</Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+
+        {db.teams.enableEditing ? (
+          <Button variant="contained" color="primary">
+            Add
+      </Button>) : ""}
+      </TabPanel>
+
+
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </div>
+  );
+}
