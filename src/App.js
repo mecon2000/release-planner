950a56c2e069e2d37d18f6c5e47ff2480c31a67c
Ronnie Ganot <rganot@aligntech.com>
2020-01-11 19:47:39 +0200

    separating TabPanel to its own file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+
 import { cloneDeep } from 'lodash';
 
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
 import './App.css';
+import { TabPanel } from './TabPanel.js';
 import { initialDb } from './release_planner_db.js';
 import { DataAdder } from './DataAdder.js';
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
-
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
 
 function a11yProps(index) {
   return {
